c1fe32afd3697b4671d4628db33219572d072bad
Reene <reenecondori0212@gmail.com>
2024-04-21 18:10:57 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicios1/DivisionCero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios1
+{
+    internal class DivisionCero
+    {
+        public static void Dividir(int dividendo, int divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                int resultado = dividendo / divisor;
+                Console.WriteLine($"El resultado de la división es: {resultado}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicios1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Ejercicios1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            DivisionCero.Dividir(10, 0);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("La division entre cero no esta permitida.");
+        }
+        finally
+        {
+            Console.WriteLine("Fin del programa");
+        }
+
+        Console.WriteLine("Presiona cualquier tecla para cerrar la consola");
+        Console.ReadLine();
+    }
+}
